ac6dbc762d4d0a1054be7cece6c7b6313d6089bf
Dibakar <dibakarbera01@gmail.com>
2024-08-03 21:12:29 +0530

    chnage color

--- frontend/src/pages/home/component/Hero.jsx
+++ frontend/src/pages/home/component/Hero.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <section className="flex flex-col lg:flex-row dark:text-slate-100 items-center">
+    <section className="flex flex-col lg:flex-row dark:text-blue-600 items-center">
       <div className="text my-5">
         <h1 className="text-5xl font-bold">The Ultimate eBook Store</h1>
-        <p className="text-2xl my-7 px-1 dark:text-slate-300">
+        <p className="text-2xl my-7 px-1 dark:text-yellow-300">
           CodeBook is the world's most popular and authoritative source for
           computer science ebooks. Find ratings and access to the newest books
           digitally.
